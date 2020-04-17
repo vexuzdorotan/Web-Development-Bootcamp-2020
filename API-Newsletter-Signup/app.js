da04e98e394916bde15443936c2184b863b52ff2
VeXuZ <vexuzdorotan@gmail.com>
2020-04-17 22:32:33 +0800

    Newsletter Signup

---
+++ API-Newsletter-Signup/app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/signup.html`);
+});
+
+app.post("/", (req, res) => {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+            },
+        }, ],
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us4.api.mailchimp.com/3.0/lists/097e2b103a";
+    const options = {
+        method: "POST",
+        auth: "user:a3f2c392b3c3f36a2f0692c8dbe56569f-us4",
+    };
+
+    const request = https.request(url, options, (response) => {
+        if (response.statusCode === 200) {
+            res.sendFile(`${__dirname}/success.html`);
+        } else {
+            res.sendFile(`${__dirname}/failure.html`);
+        }
+
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(port || 3000, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// API Key
+// 3f2c392b3c3f36a2f0692c8dbe56569f-us4
+
+// List ID
+// 097e2b103a
